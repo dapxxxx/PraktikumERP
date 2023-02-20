2ce9f9575f0517f91f735894ad72980b48399b84
dapxxxx <daffaandh87@gmail.com>
2023-02-20 11:20:20 +0700

    prak1

---
+++ Praktikum01/__manifest__.py
@@ -0,0 +1,27 @@
+{
+    "name"              : "New Addons",
+    "version"           : "1.0",
+    "author"            : "Daffa Andhika Ramadhan",
+    "website"           : "https://www.detik.com/",
+    "category"          : "New Module",
+    "summary"           : "Ini adalah addons baru",
+    "description"       : """
+     Latihan membuat addons baru di odoo 1 """,
+    "depends"           : [
+     "base",
+    ],
+    "data0"             : [
+
+    ],
+    "demo"              : [],
+    "test"              : [],
+    "image"             : [],
+    "qweb"              : [],
+    "css"               : [],
+    "application"       : [],
+    "installable"       : [],
+    "auto_install"      : [],
+    "applications"      : True,
+    "installable"       : True,
+    "auto_install"      : False,
+}
